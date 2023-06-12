97898312f9ead6e568789b380ebef061f5147aaa
JPcookie290 <julia.paier@yahoo.com>
2023-06-12 13:26:31 +0200

    new beginnings

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 "use strict";
 
-
+const keys = document.querySelector('#digits')
+const display = document.querySelector('#display')
+let num1 = 0;
+let num2 = 0;
 
 function calculate(x, operator, y) {
+    console.log(x, y);
     if (operator === '+') {
         return x + y;
     } else if (operator === '-') {
@@ -12,16 +16,9 @@ function calculate(x, operator, y) {
     } else if (operator === '/') {
         return x / y;
     }
+    console.log(y);
     return y;
 }
-
-const keys = document.querySelector('#digits')
-const display = document.querySelector('#display')
-let current = "";
-let checkNum2 = false;
-let zahl1;
-let zahl2;
-let ergebnis;
 
 function listenToKeys() {
     keys.addEventListener('click', (e) => {
@@ -31,8 +28,14 @@ function listenToKeys() {
             let num = key.dataset.value;
 
             if (!action) {
-                numLog(num);
-                updateDisplay();
+                console.log("number");
+                if(num2 === 0){
+                    num1 = num;
+                } 
+                if (num1 != 0){
+                    num2 = num;
+                }
+                console.log("numbers", num1, num2);
             }
 
             if ((action === '+') ||
@@ -40,68 +43,16 @@ function listenToKeys() {
                 (action === '*') ||
                 (action === '/') ||
                 (action === '=')) {
-                opLog(action);
-                updateDisplay();
+                    console.log("operator");
             }
 
             if (action == '.') {
-                addPoint();
-                updateDisplay();
+                console.log("dot");
             }
 
             if (action == 'clear') {
-                clear();
-                updateDisplay();
+                console.log("clear");
             }
         }
     })
 }
-
-function updateDisplay() {
-    display.innerHTML = current;
-}
-
-function numLog(zahl) {
-    if (checkNum2 === false) {
-        current += zahl;
-    }
-    if (checkNum2 === true) {
-        current += zahl;
-    }
-    //console.log('Ziffer', current, checkNum2);
-}
-
-function opLog(operator) {
-    if (checkNum2 === false) {
-        zahl1 = parseFloat(current);
-        checkNum2 = true;
-        current = "";
-    }
-    if (checkNum2 === true) {
-        zahl2 = parseFloat(current);
-        if (isNaN(ergebnis)) {
-            ergebnis = calculate(zahl1, operator, zahl2);
-            console.log(zahl1, zahl2, ergebnis);
-            current = "";
-        } else{
-            zahl1 = ergebnis;
-            ergebnis = calculate(zahl1, operator, zahl2);
-            console.log(zahl1, zahl2, ergebnis);
-            current = "";
-        }
-    }
-    //console.log('Rechenoperator', zahl1, operator, checkNum2);
-}
-
-function addPoint() {
-    current += ".";
-    console.log('Punkt');
-}
-
-function clear() {
-    current = "";
-    checkNum2 = false;
-    console.log('AC', checkNum2);
-}
-
-listenToKeys();
